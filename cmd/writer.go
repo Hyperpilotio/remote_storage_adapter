cfc364cf262a2d7442f3d83dbfb7e35c2a9d5380
alan.yang <jpra1113@gmail.com>
2018-02-02 15:33:18 +0800

    Use sendSamples to write data

--- cmd/writer.go
+++ cmd/writer.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"time"
 
 	"github.com/cenkalti/backoff"
@@ -26,16 +27,16 @@ func NewHyperpilotWriter(server *Server, w writer, customerId string) (*Hyperpil
 	}, nil
 }
 
-func (writer *HyperpilotWriter) Run() {
-	timeOut := writer.Server.Config.GetString("writer.timeOut")
+func (hpwriter *HyperpilotWriter) Run() {
+	timeOut := hpwriter.Server.Config.GetString("writer.timeOut")
 	retryTimeout, err := time.ParseDuration(timeOut)
 	if err != nil {
 		log.Warnf("Parse WriterTimeOut {%s} fail, use default interval 3 min in writer {%s}",
-			timeOut, writer.CustomerId)
+			timeOut, hpwriter.CustomerId)
 		retryTimeout = 3 * time.Minute
 	}
 
-	batchSize := writer.Server.Config.GetInt("writer.batchSize")
+	batchSize := hpwriter.Server.Config.GetInt("writer.batchSize")
 	if batchSize < 1 {
 		log.Warnf("Batch Size {%d} is not feasible, use 1 instead", batchSize)
 		batchSize = 1
@@ -48,36 +49,29 @@ func (writer *HyperpilotWriter) Run() {
 		b.MaxElapsedTime = retryTimeout
 
 		for {
-			if !writer.Queue.Empty() {
-				var batchSamples model.Samples
-				for i := 0; i < batchSize; i++ {
-					sample := writer.Queue.Dequeue()
-					if sample == nil {
-						log.Warnf("Writer {%s} get nil sample, because element number inside of queue is less than batch size {%d}",
-							writer.CustomerId, batchSize)
-						continue
+			if !hpwriter.Queue.Empty() {
+				go func(customerId string, w writer) {
+					var batchSamples model.Samples
+					for i := 0; i < batchSize; i++ {
+						sample := hpwriter.Queue.Dequeue()
+						if sample == nil {
+							log.Warnf("Writer {%s} get nil sample, because element number inside of queue is less than batch size {%d}",
+								hpwriter.CustomerId, batchSize)
+							continue
+						}
+						batchSamples = append(batchSamples, sample.(model.Samples)...)
 					}
-					batchSamples = append(batchSamples, sample.(model.Samples)...)
-				}
 
-				retryWrite := func() error {
-					writerName := writer.Writer.Name()
-					begin := time.Now()
-					err := writer.Writer.Write(batchSamples)
-					duration := time.Since(begin).Seconds()
+					retryWrite := func() error {
+						return sendSamples(w, batchSamples)
+					}
+
+					err := backoff.Retry(retryWrite, b)
 					if err != nil {
-						failedSamples.WithLabelValues(writerName).Add(float64(len(batchSamples)))
+						log.Warnf("Writer {%s} push sample fail, %d sample are dropped: %s", customerId, len(batchSamples), err.Error())
 					}
-					sentSamples.WithLabelValues(writerName).Add(float64(len(batchSamples)))
-					sentBatchDuration.WithLabelValues(writerName).Observe(duration)
-					return err
-				}
-
-				err := backoff.Retry(retryWrite, b)
-				if err != nil {
-					log.Warnf("Writer {%s} push sample fail, %d sample are dropped: %s", writer.CustomerId, len(batchSamples), err.Error())
-				}
-				time.Sleep(1 * time.Second)
+				}(hpwriter.CustomerId, hpwriter.Writer)
+				// time.Sleep(1 * time.Second)
 			}
 		}
 	}()
@@ -85,4 +79,18 @@ func (writer *HyperpilotWriter) Run() {
 
 func (writer *HyperpilotWriter) Put(samples model.Samples) {
 	writer.Queue.Enqueue(samples)
+}
+
+func sendSamples(w writer, samples model.Samples) error {
+	begin := time.Now()
+	err := w.Write(samples)
+	duration := time.Since(begin).Seconds()
+	sentSamples.WithLabelValues(w.Name()).Add(float64(len(samples)))
+	sentBatchDuration.WithLabelValues(w.Name()).Observe(duration)
+	if err != nil {
+		failedSamples.WithLabelValues(w.Name()).Add(float64(len(samples)))
+		return errors.New("Unable to sending samples to remote storage:" + err.Error())
+	}
+
+	return nil
 }
