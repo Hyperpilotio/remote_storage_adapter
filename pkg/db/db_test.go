a93376cb4e8755cfc8a6731c5d2d7c8070c91d8e
alan.yang <jpra1113@gmail.com>
2018-02-02 11:09:04 +0800

    Add mongo authdb test

--- pkg/db/db_test.go
+++ pkg/db/db_test.go
@@ -7,16 +7,21 @@ import (
 	"github.com/spf13/viper"
 )
 
-func TestAuthDB(t *testing.T) {
-	config := viper.New()
+var authdb *AuthDB
+var config *viper.Viper
+
+func init() {
+	config = viper.New()
 	config.SetConfigType("json")
-	config.Set("database.url", "localhost:27017")
+	config.Set("database.url", "ds117758.mlab.com:17758")
 	config.Set("database.user", "analyzer")
 	config.Set("database.password", "hyperpilot")
 	config.Set("database.configDatabase", "authdb")
 	config.Set("database.customerCollection", "customers")
-	authdb := NewAuthDB(config)
+	authdb = NewAuthDB(config)
+}
 
+func TestWriteMetrics(t *testing.T) {
 	customerConfig := &hpmodel.CustomerConfig{
 		Token:      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
 		CustomerId: "hyperpilotio",
@@ -31,5 +36,12 @@ func TestAuthDB(t *testing.T) {
 	}
 	if err := authdb.WriteMetrics("customer", customerConfig); err != nil {
 		t.Error("Unable write customer data to mongo")
+	}
+}
+
+func TestGetCustomers(t *testing.T) {
+	_, err := authdb.GetCustomers()
+	if err != nil {
+		t.Error("Unable get customers from mongo")
 	}
 }
